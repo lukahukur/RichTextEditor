6a19558f103e6d434a5cd364c9b6e2b49305eefa
lukahukur <6wingman9@gmail.com>
2023-03-29 20:20:15 +0400

    beta

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(
+        __dirname,
+        'src/components/TextEditor/index.ts',
+      ),
+      name: 'RTE-Minimalistic',
+      fileName: (f) => `index.${f}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts()],
 })
